86f10eb08a87680de594a91add738bb8e6675579
zorkow <volker.sorge@gmail.com>
2015-09-17 16:56:38 +0100

    Finds the correct root element for speech generation.

--- Semantics-Lab/Assistive-Explore.js
+++ Semantics-Lab/Assistive-Explore.js
@@ -1,24 +1,30 @@
 
 
 (function() {
-  var FALSE, HOVER, KEY;
+  var FALSE, KEY;
   MathJax.Hub.Register.StartupHook("MathEvents Ready",function () {
     FALSE = MathJax.Extension.MathEvents.Event.False;
-    HOVER = MathJax.Extension.MathEvents.Hover;
     KEY = MathJax.Extension.MathEvents.Event.KEY;
   });
+
   var Explorer = MathJax.Extension.Explorer = {
     enriched: [],
     active: [],
     speechDiv: null,
+    //
+    // Collates all enriched jaxs.
+    //
     Register: function(jax, id, script) {
       if (jax.enriched) {
         Explorer.enriched.push(jax);
       };
     },
+    //
+    // Adds key events to each enriched jax.
+    //
     AddEvents: function() {
       for (var i = 0, jax; jax = Explorer.enriched[i]; i++) {
-        // This is costly! Can I do this also from the script?
+        // This is costly! Can I do this also by walking back from script?
         var frame = document.getElementById(jax.inputID + '-Frame');
         if (frame) {
           Explorer.AddEvent(frame);
@@ -26,18 +32,24 @@
         }
       }
       if (Explorer.enriched.length !== Explorer.active.length) {
-          MathJax.Hub.Queue(["AddEvents", Explorer]);
+        MathJax.Hub.Queue(["AddEvents", Explorer]);
       }
     },
+    //
+    // Adds a single key event.
+    //
     AddEvent: function(element) {
       element.onkeydown = Explorer.Keydown;
     },
+    // 
+    // Event execution on keydown. Subsumes the same method of MathEvents.
+    //
     Keydown: function (event) {
       if (event.keyCode === KEY.SPACE) {
         var math = event.target;
         if (event.shiftKey) {
           Explorer.AddSpeech(math);
-          var speechNode = math.querySelector('span[data-semantic-speech]');
+          var speechNode = Explorer.GetRoot(math);
           Explorer.Speak(speechNode.getAttribute('data-semantic-speech'));
         } else {
           MathJax.Extension.MathEvents.Event.ContextMenu(event, math);
@@ -47,6 +59,9 @@
       }
       FALSE(event);
     },
+    //
+    // Adds the speech div.
+    //
     AddSpeech: function(math) {
       if (!Explorer.speechDiv) {
         Explorer.speechDiv = MathJax.HTML.addElement(
@@ -55,19 +70,32 @@
         Explorer.speechDiv.setAttribute('aria-live', 'assertive');
       }
     },
+    //
+    // Removes the speech div.
+    //
     RemoveSpeech: function() {
       if (Explorer.speechDiv) {
         Explorer.speechDiv.parentNode.removeChild(Explorer.speechDiv);
       }
       Explorer.speechDiv = null;
     },
+    //
+    // Speaks a string by poking it into the speech div.
+    //
     Speak: function(speech) {
       Explorer.speechDiv.textContent = speech;
     },
+    //
+    //
+    //
     GetRoot: function(math) {
       var speechNodes = math.querySelectorAll('span[data-semantic-speech]');
-      var parent = speechNodes.find(function(x) {
-          return !x.hasAttribute('data-semantic-parent');});
+      for (var i = 0, speechNode; speechNode = speechNodes[i]; i++) {
+        if (!speechNode.hasAttribute('data-semantic-parent')) {
+          return speechNode;
+        }
+      }
+      return speechNodes[0];
     }
   };
   
