a6a614b9c1dbae30b29aeb5adf7c95f1c32a084a
zorkow <volker.sorge@gmail.com>
2016-05-09 17:28:53 +0100

    Adapts code to usual MathJax style.

--- extensions/Accessibility-Extension.js
+++ extensions/Accessibility-Extension.js
@@ -1,85 +1,87 @@
 //
 // Thin hook to include the accessibility extension.
 //
-MathJax.Extension.Accessibility = {
-  version: '1.0',
-  default: {
-    explorer: false,
-    collapse: false
-  },
-  AddDefaults: function() {
-    var settings = MathJax.Hub.config.menuSettings;
-    var keys = Object.keys(MathJax.Extension.Accessibility.default);
-    for (var i = 0, key; key = keys[i]; i++) {
-      if (typeof(settings['Accessibility-' + key]) === 'undefined') {
-        settings['Accessibility-' + key] =
-          MathJax.Extension.Accessibility.default[key];
+(function(HUB) {
+  var SETTINGS = HUB.config.menuSettings;
+  var ITEM = MathJax.Menu.ITEM;
+  var MENU = MathJax.Menu.menu;
+  
+  var Accessibility = MathJax.Extension.Accessibility = {
+    version: '1.0',
+    default: {
+      explorer: false,
+      collapse: false
+    },
+    AddDefaults: function() {
+      var keys = Object.keys(Accessibility.default);
+      for (var i = 0, key; key = keys[i]; i++) {
+        if (typeof(SETTINGS['Accessibility-' + key]) === 'undefined') {
+          SETTINGS['Accessibility-' + key] =
+            Accessibility.default[key];
+        }
+      }
+    },
+    GetOption: function(option) {
+      return SETTINGS['Accessibility-' + option];
+    },
+    SwitchExplorer: function() {
+      Accessibility.LoadExplorer();
+    },
+    LoadModule: function(option, module, signal, opt_startup) {
+      var flag = Accessibility.GetOption(option);
+      if (flag) {
+        MathJax.Ajax.Require(module);
+      }
+      if (opt_startup) return flag;
+      HUB.Register.StartupHook(signal, function() {
+        HUB.Reprocess();
+      });
+      return flag;
+    },
+    LoadExplorer: function(opt_startup) {
+      return Accessibility.LoadModule(
+        'explorer', '[RespEq]/Assistive-Explore.js', 'Explorer Ready', opt_startup
+      );
+    },
+    LoadCollapse: function(opt_startup) {
+      return Accessibility.LoadModule(
+        'collapse', '[RespEq]/Semantic-Collapse.js', 'Semantic Collapse Ready',
+        opt_startup
+      );
+    },
+    SwitchCollapse: function() {
+      Accessibility.LoadCollapse();
+    },
+    Startup: function() {
+      Accessibility.AddDefaults();
+      Accessibility.Menu.Add();
+      var explorer = Accessibility.LoadExplorer(true);
+      var collapse = Accessibility.LoadCollapse(true);
+      if (explorer || collapse) {
+        HUB.Queue(['Reprocess', HUB]);
       }
     }
-  },
-  GetOption: function(option) {
-    return MathJax.Hub.config.menuSettings['Accessibility-' + option];
-  },
-  AddMenu: function() {
-    var ITEM = MathJax.Menu.ITEM;
-    var accessibilityBox =
-          ITEM.CHECKBOX(['Accessibility', 'Accessibility'],
-                        'Accessibility-explorer',
-                        {action: MathJax.Extension.Accessibility.SwitchExplorer});
-    var responsiveBox =
-          ITEM.CHECKBOX(['ResponsiveEquations', 'Responsive Equations'],
-                        'Accessibility-collapse',
-                        {action: MathJax.Extension.Accessibility.SwitchCollapse});
+  };
 
-    // Attaches the menu;
-    var about = MathJax.Menu.menu.IndexOfId('About');
-    if (about === null) {
-      MathJax.Menu.menu.items.push(
-        ITEM.RULE(), responsiveBox, accessibilityBox);
-      return;
+  var A11YMENU = MathJax.Extension.Accessibility.Menu = {
+    a11yBox: ITEM.CHECKBOX(['AccessibilityBox', 'Accessibility'],
+                           'Accessibility-explorer',
+                           {action: Accessibility.SwitchExplorer}),
+    respBox: ITEM.CHECKBOX(['ResponsiveBox', 'Responsive Equations'],
+                                 'Accessibility-collapse',
+                           {action: Accessibility.SwitchCollapse}),
+    Add: function() {
+      // Attaches the menu;
+      var about = MENU.IndexOfId('About');
+      about === null ? 
+        MENU.items.push(ITEM.RULE(), A11YMENU.respBox, A11YMENU.a11yBox) :
+        MENU.items.splice(
+          about, 0, A11YMENU.respBox, A11YMENU.a11yBox, ITEM.RULE());
     }
-    MathJax.Menu.menu.items.splice(
-      about, 0, responsiveBox, accessibilityBox, ITEM.RULE());
-    
-  },
-  LoadModule: function(option, module, signal, opt_startup) {
-    var flag = MathJax.Extension.Accessibility.GetOption(option);
-    if (flag) {
-      MathJax.Ajax.Require(module);
-    }
-    if (opt_startup) return flag;
-    MathJax.Hub.Register.StartupHook(signal, function() {
-      MathJax.Hub.Reprocess();
-    });
-    return flag;
-  },
-  LoadExplorer: function(opt_startup) {
-    return MathJax.Extension.Accessibility.LoadModule(
-      'explorer', '[RespEq]/Assistive-Explore.js', 'Explorer Ready', opt_startup
-    );
-  },
-  SwitchExplorer: function() {
-    MathJax.Extension.Accessibility.LoadExplorer();
-  },
-  LoadCollapse: function(opt_startup) {
-    return MathJax.Extension.Accessibility.LoadModule(
-      'collapse', '[RespEq]/Semantic-Collapse.js', 'Semantic Collapse Ready',
-      opt_startup
-    );
-  },
-  SwitchCollapse: function() {
-    MathJax.Extension.Accessibility.LoadCollapse();
-  },
-  Startup: function() {
-    MathJax.Extension.Accessibility.AddDefaults();
-    MathJax.Extension.Accessibility.AddMenu();
-    var explorer = MathJax.Extension.Accessibility.LoadExplorer(true);
-    var collapse = MathJax.Extension.Accessibility.LoadCollapse(true);
-    if (explorer || collapse) {
-      MathJax.Hub.Queue(['Reprocess', MathJax.Hub]);
-    }
-  }
-};
+  };
+
+})(MathJax.Hub);
+
 
 
 MathJax.Callback.Queue(
