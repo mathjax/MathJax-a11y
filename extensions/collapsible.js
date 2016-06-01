1a7193ceec606861a167c475e94295b512caabbf
Davide P. Cervone <dpvc@union.edu>
2016-06-01 17:56:41 -0400

    Add copyright notices to the extensions

--- extensions/collapsible.js
+++ extensions/collapsible.js
@@ -1,10 +1,30 @@
-//
-//  A filter to add maction elements to the enriched MathML for parts that
-//  can be collapsed.  We determine this based on a "complexity" value and
-//  collapse those terms that exceed a given complexity.
-//
-//  The parameters controlling the complexity measure still need work.
-//
+/*************************************************************
+ *
+ *  [Contrib]/a11y/collapsible.js
+ *  
+ *  A filter to add maction elements to the enriched MathML for parts that
+ *  can be collapsed.  We determine this based on a "complexity" value and
+ *  collapse those terms that exceed a given complexity.
+ *
+ *  The parameters controlling the complexity measure still need work.
+ *
+ *  ---------------------------------------------------------------------
+ *  
+ *  Copyright (c) 2016 The MathJax Consortium
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 (function (HUB) {
   var MML;
   
