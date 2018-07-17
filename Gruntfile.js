7b336ac99f0f190a42d2bfe1610b9a00acf7a150
zorkow <volker.sorge@gmail.com>
2018-07-17 18:31:26 +0100

    Changes to grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,16 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    'json-minify': {
-      build: {
-        files: 'dist/mathmaps/*/*.js'
-      }
-    },
     gitclone: {
       'speech-rule-engine': {
         options: {
           repository: 'https://github.com/zorkow/speech-rule-engine.git',
-          branch: 'v2.2.1',
+          branch: 'v2.2.2',
           cwd: '.'
         }
       }
     },
     'uglify': {
-      mathmaps: {
-        files: {
-          'dist/mathmaps/mathmaps_ie.js': 'dist/mathmaps/mathmaps_ie.js'
-        }
-      },
       build: {
         files: {
           'dist/accessibility-menu.js': 'extensions/accessibility-menu.js',
@@ -67,7 +57,9 @@ module.exports = function(grunt) {
           'npm install',
           'npm install --only=dev',
           'npm install wicked-good-xpath',
-          'make mathjax'
+          'make mathjax',
+          'make maps',
+          'make iemaps'
         ].join('&&'),
         options: {
           execOptions: {
@@ -78,11 +70,9 @@ module.exports = function(grunt) {
       copy: [
         'cp speech-rule-engine/lib/mathjax-sre.js dist/mathjax-sre.js',
         'cp speech-rule-engine/node_modules/wicked-good-xpath/dist/wgxpath.install.js dist/',
-        'cp -R speech-rule-engine/src/mathmaps dist/',
+        'cp -R speech-rule-engine/lib/mathmaps dist/',
         'cp extensions/*.ogg dist/',
-        'cp extensions/*.mp3 dist/',
-        'rm dist/mathmaps/math_map.js',
-        'rm dist/mathmaps/.htaccess'
+        'cp extensions/*.mp3 dist/'
       ].join('&&')
     }
   });
@@ -91,8 +81,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-git');
   // Run shell commands.
   grunt.loadNpmTasks('grunt-shell');
-  // Minify JSON files.
-  grunt.loadNpmTasks('grunt-json-minify');
   // Minify regular files.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
@@ -101,7 +89,6 @@ module.exports = function(grunt) {
     'gitclone',
     'shell:compile',
     'shell:copy',
-    'json-minify',
     'uglify',
     'shell:clean_sre'
   ]);
