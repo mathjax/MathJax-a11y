c67561bed737de5587e5a394419eaa57a4025b9f
zorkow <volker.sorge@gmail.com>
2020-06-27 17:55:45 +0100

    Version bump.

--- extensions/collapsible.js
+++ extensions/collapsible.js
@@ -41,7 +41,7 @@
   if (!PATH.a11y) PATH.a11y = HUB.config.root + "/extensions/a11y";
 
   var Collapsible = MathJax.Extension.collapsible = {
-    version: "1.5.0",
+    version: "1.6.0",
     config: HUB.CombineConfig("collapsible",{
       disabled: false
     }),
